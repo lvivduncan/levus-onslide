b2813565640d20a125119934e484e783ae2fba1a
lvivduncan <lvivduncan@gmail.com>
2020-05-07 11:21:19 +0300

    add animation to arrow

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 const slider = document.querySelector('#levus-slider');
 
 // елементи
-const slides = document.querySelectorAll('#levus-slider article');
+const slides = slider.querySelectorAll('article');
 
 // вліво
-const left = document.querySelector('#levus-left');
+const left = slider.querySelector('#levus-left');
 
 // вправо
-const right = document.querySelector('#levus-right');
+const right = slider.querySelector('#levus-right');
 
 // лічильник
 let cnt = 0;
@@ -35,3 +35,47 @@ right.addEventListener('click', () => {
     slides[cnt].classList.add('show');
   }, 500);
 });
+
+// показуємо стрілочки при завантаженні
+document.addEventListener('DOMContentLoaded', () => {
+  if(window.innerWidth > 1200){
+    hideArrow();
+  } else {
+    showArrow();
+  }
+});
+
+// показуємо при зміні розміру екрану
+window.addEventListener('resize', () => {
+  if(window.innerWidth > 1200){
+    hideArrow();
+  } else {
+    showArrow();
+  }
+});
+
+// сховати стрілки
+function hideArrow(){
+
+  // ховаємо
+  left.classList.add('hide');
+  right.classList.add('hide');
+
+  // показати стрілки при наведенні на слайдер
+  slider.addEventListener('mouseover', () => {
+      left.removeAttribute('class');
+      right.removeAttribute('class');
+  });
+  
+  // сховати стрілки
+  slider.addEventListener('mouseout', () => {
+    left.classList.add('hide');
+    right.classList.add('hide');
+  });
+}
+
+// показати стрілки
+function showArrow(){
+  left.removeAttribute('class');
+  right.removeAttribute('class');
+}
